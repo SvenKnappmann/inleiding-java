6a31045a6f624ccc2bbd1ff7010a44eaab6a0d3f
SvenKnappmann <SKnappmann@Roc-dev.com>
2020-10-06 15:21:51 +0200

    added H10_1

---
+++ src/h11/Opdracht11_1.java
@@ -0,0 +1,19 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht11_1 extends Applet{
+    public void init() {
+
+    }
+    public void paint(Graphics g){
+        int teller = 0;
+        int x = 50;
+        do {
+            x += 20;
+            g.drawLine(x,30,x,200);
+            teller++;
+        } while (teller < 10);
+    }
+}
