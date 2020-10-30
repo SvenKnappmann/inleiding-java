79841efac5d0cccba39ae371380655941194a46a
SvenKnappmann <SKnappmann@Roc-dev.com>
2020-10-30 09:57:54 +0100

    changed h14_1

--- src/h14/Opdracht14_1.java
+++ src/h14/Opdracht14_1.java
@@ -10,11 +10,9 @@ public class Opdracht14_1 extends Applet {
     String[] arrayString = {"Aas","2","3","4","5","6","7","8","9","10","Boer","Vrouw","Heer"};
 
     Button deelKaart;
-    String warning = "";
 
     int kaartKleur;
     int kaartNummer;
-
 
 
     public void init() {
@@ -22,24 +20,23 @@ public class Opdracht14_1 extends Applet {
         deelKaart = new Button("Deel kaart");
         deelKaart.addActionListener(new DeelKaart());
         add(deelKaart);
-        warning = "WARNING";
-
 
 
     }
     public void paint(Graphics g) {
 
         g.drawString("" + arrayColor[kaartKleur] + " " + arrayString[kaartNummer],30,30);
-        g.drawString(warning,30,45);
+
 
     }
     class DeelKaart implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
             kaartKleur = (int) (Math.round(Math.random() * 3));
-            kaartNummer = (int) (Math.round(Math.random() * 13));
-            warning = "";
+            kaartNummer = (int) (Math.round(Math.random() * 12));
             repaint();
+
+
         }
     }
 }
